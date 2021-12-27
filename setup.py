beacf576a026fddfb9a6febd69458461cc437a2a
Felipe Delestro <delestro@gmail.com>
2021-12-27 11:35:37 +0100

    Basics for PyPi

--- setup.py
+++ setup.py
@@ -1,21 +1,23 @@
+from setuptools import setup
 
-from setuptools import setup, find_packages
 
-
-with open('README.md') as f:
+with open("README.md") as f:
     readme = f.read()
 
-with open('LICENSE') as f:
+with open("LICENSE") as f:
     license = f.read()
 
 setup(
-    name='outputformater',
-    version='0.0.1',
-    description='Decorate and beautify strings',
+    name="outputformater",
+    packages=["outputformater"],
+    version="0.1.0",
+    description="Decorate and beautify output",
     long_description=readme,
-    author='Felipe Delestro',
-    author_email='delestro@gmail.com',
-    url='https://github.com/delestro/outputformater',
+    author="Felipe Delestro",
+    author_email="delestro@gmail.com",
+    url="https://github.com/delestro/outputformater",
+    download_url="https://github.com/delestro/outputformater/archive/refs/tags/v0.1.0.tar.gz",
     license=license,
-    packages=find_packages()
+    keywords=["output", "print", "decorate", "format", "string", "beautify"],
+    python_requires=">=3.6",
 )
