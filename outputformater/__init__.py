1f9ea02d52fb6dfa47137e41c14a3f3afca056ec
Felipe Delestro <delestro@gmail.com>
2021-12-30 14:00:38 +0100

    renaming everything from outputformater to outputformat

--- outputformater/__init__.py
+++
@@ -1,6 +0,0 @@
-from outputformater.title import boxtitle
-from outputformater.title import linetitle
-from outputformater.title import bigtitle
-from outputformater.list import showlist
-from outputformater.plot import bar
-from outputformater.plot import barlist
